8abf36e6dacc248b5c66cb5ad7bd417acbd2b03d
Andrey Zhukov <a.m.zhukov@inbox.ru>
2019-09-06 22:42:28 +0500

    second-commit

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 'use strict';
+
+let newElem = document.createElement('li');
+newElem.textContent = 'Глава 8: За пределами ES6';
+document.querySelectorAll('ul')[2].appendChild(newElem);
+document.querySelectorAll('ul')[2].insertBefore(document.querySelectorAll('li')[27], document.querySelectorAll('li')[26]);
+
+
+
+
+let elemUl = document.querySelectorAll('ul'),//elemUl = document.querySelectorAll('ul'), // Create selector to UL, tor transport inside il elements
+    elemLi = document.querySelectorAll('li'), // Create selector to inside elements, tor transport inside li elements
+
+    collectRoot = document.querySelector('body'),
+    collect = document.querySelectorAll('.books'), //Create selector to collection, for transport elements.
+    elem = document.querySelectorAll('.book'), // Create selector to elements, for transport
+    elemH2 = document.querySelectorAll('h2'),
+    adv = document.querySelector('.adv'); // Create selector to advertising
+    
+    //console.log(document.querySelectorAll('li'));
+
+    
+    console.log(document.querySelectorAll('li'));
 //Восстановить порядок книг.
-let collectRoot = document.querySelector('body');
-let collect = document.querySelectorAll('.books'); //Create selector to collection, for transport elements.
-let elem = document.querySelectorAll('.book'); // Create selector to elements, for transport
-let adv = document.querySelector('.adv'); // Create selector to advertising
-let elemUl = document.querySelectorAll('ul'); // Create selector to UL, tor transport inside il elements
-let elemLi = document.querySelectorAll('li'); // Create selector to inside elements, tor transport inside li elements
-
-
-
 collect[0].insertBefore(elem[0], elem[2]); // transport null and first elements.
 collect[0].insertBefore(elem[2], elem[6]); // transport second and last elements.
 collect[0].insertBefore(elem[3], elem[5]); // transport third and fourth elements.
 
-collectRoot.setAttribute('style', 'background-image: url(./image/you-dont-know-js.jpg)'); //change background image.
+//change background image.
+collectRoot.setAttribute('style', 'background-image: url(./image/you-dont-know-js.jpg)'); 
 
+//Замена пропопипов на прототипов
 document.querySelectorAll('h2')[2].innerHTML = '<a href="https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes"\
 target="_blank">Книга 3. this и Прототипы Объектов</a>'; //chage h2.
 
 collectRoot.removeChild(adv); // This is a function to remove advertising.
 
-//elemUl[1].insertBefore(elemLi[2], elemLi[10]);
-elemUl[3].insertBefore(elemLi[2], elemLi[4]);
+//Сортировка глав, внутри книги
 document.querySelectorAll('ul')[1].insertBefore(elemLi[2], elemLi[10]);
+document.querySelectorAll('ul')[1].insertBefore(elemLi[6], elemLi[4]); //sort elements of li
+document.querySelectorAll('ul')[1].insertBefore(elemLi[8], elemLi[4]);
+document.querySelectorAll('ul')[4].insertBefore(elemLi[56], elemLi[49]); //sort elements of li
+document.querySelectorAll('ul')[4].insertBefore(elemLi[56], elemLi[50]);
+document.querySelectorAll('ul')[4].insertBefore(elemLi[52], elemLi[55]);
+document.querySelectorAll('ul')[4].insertBefore(elemLi[49], elemLi[53]);
 
 
+
+console.log(document.querySelectorAll('li'));
